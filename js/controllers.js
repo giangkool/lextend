a6b67eb2dd1f5e56a999351404c6e40d9c65069e
Huy Nguyen Quang <huyroberto@gmail.com>
2016-07-06 14:32:14 +0700

    Display format for today word

--- js/controllers.js
+++ js/controllers.js
@@ -1,3 +1,31 @@
+String.prototype.replaceAll = function( token, newToken, ignoreCase ) {
+    var _token;
+    var str = this + "";
+    var i = -1;
+
+    if ( typeof token === "string" ) {
+
+        if ( ignoreCase ) {
+
+            _token = token.toLowerCase();
+
+            while( (
+                i = str.toLowerCase().indexOf(
+                    _token, i >= 0 ? i + newToken.length : 0
+                ) ) !== -1
+            ) {
+                str = str.substring( 0, i ) +
+                    newToken +
+                    str.substring( i + token.length );
+            }
+
+        } else {
+            return this.split( token ).join( newToken );
+        }
+
+    }
+return str;
+};
 var app  = angular.module('Lextend.controller', ['ngRoute', 'ngStorage', 'dataServices', 'ui.router', 'ngSanitize', 'bnx.module.facebook', 'directive.g+signin']);
 app
     
